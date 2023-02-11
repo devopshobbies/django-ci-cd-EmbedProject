f471f06f256477e729697f3a0cb777ac63addbc3
amirbahador <amirbahador.pv@gmail.com>
2023-02-11 17:47:52 +0200

    Remove useless urls

--- devopshobbies/api/urls.py
+++ devopshobbies/api/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path(
-        'auth/', include(('embed.authentication.urls', 'authentication'))
-    ),
-    path('users/', include(('embed.users.urls', 'users'))),
-    path('blog/', include(('embed.blog.urls', 'blog'))),
 ]
