f064b14b61f4da58ea85efe330911d3bc8bcebfd
amirbahador <amirbahador.pv@gmail.com>
2023-02-13 03:13:29 +0200

    Add validation

--- devopshobbies/users/apis.py
+++ devopshobbies/users/apis.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import serializers
 
+from django.core.validators import MinLengthValidator
+from .validators import number_validator, special_char_validator, letter_validator
 from devopshobbies.users.models import BaseUser 
 
 from drf_spectacular.utils import extend_schema
@@ -13,8 +15,27 @@ class RegisterApi(APIView):
 
     class InputRegisterSerializer(serializers.Serializer):
         email = serializers.EmailField(max_length=255)
-        password = serializers.CharField(max_length=255)
+        password = serializers.CharField(
+                validators=[
+                        number_validator,
+                        letter_validator,
+                        special_char_validator,
+                        MinLengthValidator(limit_value=10)
+                    ]
+                )
         confirm_password = serializers.CharField(max_length=255)
+        
+        def validate_email(self, email):
+            if BaseUser.objects.filter(email=email).exists():
+                raise serializers.ValidationError("email Already Taken")
+
+        def validate(self, data):
+            if not data.get("password") or not data.get("confirm_password"):
+                raise serializers.ValidationError("Please fill password and confirm password")
+            
+            if data.get("password") != data.get("confirm_password"):
+                raise serializers.ValidationError("confirm password is not equal to password")
+
 
     class OutPutRegisterSerializer(serializers.ModelSerializer):
         class Meta:
