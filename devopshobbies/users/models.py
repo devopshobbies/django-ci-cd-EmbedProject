bb4223cc6f26d9abe8c9e1391d964e74247a8746
amirbahador <amirbahador.pv@gmail.com>
2023-02-13 02:46:55 +0200

    Add Profile Models

--- devopshobbies/users/models.py
+++ devopshobbies/users/models.py
@@ -57,3 +57,18 @@ class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
         return self.is_admin
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    posts_count = models.PositiveIntegerField(default=0)
+    subscriber_count = models.PositiveIntegerField(default=0)
+    subscription_count = models.PositiveIntegerField(default=0)
+    bio = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} >> {self.bio}"
+
+
+
+
+
+
