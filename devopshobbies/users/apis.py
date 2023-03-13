b56f866260845878e45717c600529a9e9b3169a3
amirbahador <amirbahador.pv@gmail.com>
2023-03-13 03:05:50 +0200

    FIx profile cache

--- devopshobbies/users/apis.py
+++ devopshobbies/users/apis.py
@@ -12,6 +12,7 @@ from devopshobbies.users.services import register
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 
 from drf_spectacular.utils import extend_schema
+from django.core.cache import cache
 
 
 class ProfileApi(ApiAuthMixin, APIView):
@@ -20,6 +21,16 @@ class ProfileApi(ApiAuthMixin, APIView):
         class Meta:
             model = Profile 
             fields = ("bio", "posts_count", "subscriber_count", "subscription_count")
+
+        def to_representation(self, instance):
+            rep = super().to_representation(instance)
+            cache_profile = cache.get(f"profile_{instance.user}", {})
+            if cache_profile:
+                rep["posts_count"] = cache_profile.get("posts_count")
+                rep["subscriber_count"] = cache_profile.get("subscribers_count")
+                rep["subscription_count"] = cache_profile.get("subscriptions_count")
+
+            return rep
 
     @extend_schema(responses=OutPutSerializer)
     def get(self, request):
