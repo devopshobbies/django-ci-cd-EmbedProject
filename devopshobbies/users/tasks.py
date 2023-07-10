c5ee7f0560fc020893c7a0bf40ccec2f995686b0
Hamid Rasti <43915620+hamidrasti@users.noreply.github.com>
2023-07-10 08:28:09 +0330

    alias the profile_count_update services method
    The profile_count_update service method is aliased so that it does not have the same name as the celery shared_task

--- devopshobbies/users/tasks.py
+++ devopshobbies/users/tasks.py
@@ -1,10 +1,10 @@
 from celery import shared_task
-from .services import profile_count_update
+from .services import profile_count_update as update_profile_count
 
 
 @shared_task
 def profile_count_update():
-    profile_count_update()
+    update_profile_count()
 
 @shared_task
 def hello2():
